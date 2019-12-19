eaac8ac9fc8d2016f411e953f3406989fe641585
Alain KONG <kong.alain@gmail.com>
2019-12-19 01:27:00 +0100

    ajout du place holder

--- app/javascript/components/select2.js
+++ app/javascript/components/select2.js
@@ -6,6 +6,7 @@ import 'select2-theme-bootstrap4/dist/select2-bootstrap.css'
 
 const initSelect2 = () => {
   $('.tagsSelection').select2({
+    placeholder: "Add tags",
     tags: true,
     tokenSeparators: [',', ' '],
     theme: "bootstrap"
