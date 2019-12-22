f48d7c9a9a99fc3c75d14589b0afbac6d9e6474b
Carla Dessommes <carladessommes@gmail.com>
2019-12-22 20:48:56 +0100

    added style on bookmarks and tried ajax

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,8 +3,17 @@ class FavoritesController < ApplicationController
     @favorite = Favorite.new()
     @favorite.user = current_user
     @favorite.idea = Idea.find(params[:idea_id])
-    @favorite.save
-    redirect_to idea_path(params[:idea_id])
+    if @favorite.save
+      respond_to do |format|
+        format.html { redirect_to idea_path(params[:idea_id]) }
+        format.js
+      end
+    else
+      respond_to do |format|
+        format.html { render 'ideas/show' }
+        format.js
+      end
+    end
   end
 
   def destroy
